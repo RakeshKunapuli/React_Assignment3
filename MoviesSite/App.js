142106cca9b51ceae7d897355679ae0e03595fa2
RakeshKunapuli <143953416+RakeshKunapuli@users.noreply.github.com>
2023-11-04 14:41:55 +0530

    Reupload of Assignment

---
+++ MoviesSite/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css"
+import Home from "./index"
+
+
+class App extends React.Component{
+
+render(){
+  return (
+    <>
+  <Home/>
+    </>
+  );
+}
+};
+
+export default App;
