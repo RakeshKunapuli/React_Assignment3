e109522128ffe087e798adbb2a4d348aa72bdbaf
RakeshKunapuli <143953416+RakeshKunapuli@users.noreply.github.com>
2023-11-04 14:57:11 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css"
-import Home from "./MoviesSite/index"
+import Home from "./Home"
 
 
 class App extends React.Component{
